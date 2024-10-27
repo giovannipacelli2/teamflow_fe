b38a4969f90501cfac138b0c447947c1ba75cc61
Giovanni <giovannipaceli2@gmail.com>
2024-10-27 12:52:00 +0100

    install dotenv

--- src/interceptors/interceptor.ts
+++ src/interceptors/interceptor.ts
@@ -5,7 +5,7 @@ const interceptor = () => {
   axios.interceptors.request.use(
     function (config : InternalAxiosRequestConfig) {
       
-      let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDAvYXBpL2xvZ2luIiwiaWF0IjoxNzI5OTU0NzE3LCJleHAiOjE3MzA1NTk1MTcsIm5iZiI6MTcyOTk1NDcxNywianRpIjoiaUdmczdzSmx2Zk9HYTdVUiIsInN1YiI6ImFlNjgyODNmLWRhOTMtNGYzMi1iODdmLTU1YjZjNjJjYmNlNiIsInBydiI6ImM4ZWUxZmM4OWU3NzVlYzRjNzM4NjY3ZTViZTE3YTU5MGI2ZDQwZmMiLCJyb2xlIjpudWxsfQ.ppWT45fjXsVkVPZuzyuVvRVzPfF_eTrcHiMcoOoDeM0";
+      let token = process.env.REACT_APP_HEADER;
       //token = "";
       
       config.headers.Authorization = 'Bearer ' + token;
